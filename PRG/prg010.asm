78be24d4a13351e78fe48e8eea5ad7016efec383
fortenbt <1106428+fortenbt@users.noreply.github.com>
2020-04-14 20:51:08 -0400

    Add TILE_NPCGIRL1's dialogue

--- PRG/prg010.asm
+++ PRG/prg010.asm
@@ -3976,7 +3976,7 @@ _no_npc_interaction:
 	RTS
 
 NPCTileIDList:
-	.byte TILE_NPCDOG
+	.byte TILE_NPCDOG, TILE_NPCGIRL1
 NPC_END
 
 ;;; [ORANGE] This function checks all the directions around the player on the map for
@@ -4195,7 +4195,16 @@ _npc_draw_box_cont:
 NPCMessage1:
 	.byte $00, $D8, $F0, $F0, $DA, $FF
 NPCMessage2:
-	.byte $00, $D8, $F0, $F0, $DA, $FF
+	.byte $00
+	.byte $D8, $DC, $90, $E2, $93, $91, $DC, $B4, $A9, $AA, $A8, $E0, $92, $DE, $DC, $A8, $B4, $AA, $93, $00
+	.byte $93, $AB, $A8, $FE, $AA, $93, $B1, $92, $58, $FE, $BC, $A8, $DC, $FE, $B3, $93, $AB, $00
+	.byte $FA, $DC, $A8, $DC, $FE, $AA, $93, $FE, $FA, $DC, $90, $D9, $FE, $DD, $84, $DE, $AB, $A8, $DC, $00
+	.byte $93, $AB, $AA, $FE, $B1, $FA, $E0, $AA, $B5, $A9, $FE, $DE, $93, $84, $92, $DE, $FE, $93, $92, $00
+	.byte $B1, $84, $AA, $FA, $FE, $E0, $90, $90, $FE, $AA, $FA, $DC, $A9, $DC, $FE, $93, $A8, $E1, $A9, $56, $00
+	.byte $C1, $93, $91, $DC, $FE, $A9, $E0, $B3, $FE, $F0, $90, $EF, $FE, $BA, $E0, $92, $00
+	.byte $EB, $A8, $84, $91, $91, $FE, $84, $A9, $FE, $E1, $DC, $FA, $84, $92, $EF, $FE, $84, $AA, $B4, $00
+	.byte $E1, $AB, $AA, $FE, $92, $93, $FE, $93, $92, $DC, $FE, $85, $92, $93, $B1, $A9, $00
+	.byte $B1, $FA, $E0, $AA, $FE, $AA, $FA, $DC, $B3, $FE, $E0, $A8, $DC, $58, $FF
 
 NPCMsgPtr_L:
 	.byte LOW(NPCMessage1)
@@ -4223,8 +4232,10 @@ NPC_DialogNextState:
 	INC <Map_DoNPC
 	RTS
 
+NPC_VH_ByLine:
+	.byte $29, $29, $29, $29, $29, $29, $2A, $2A, $2A
 NPC_VL_ByLine:
-	.byte $26, $46, $66, $86, $A6, $C6, $E6
+	.byte $46, $66, $86, $A6, $C6, $E6, $06, $26, $46
 
 NPC_DoDialog:
 	LDA <Map_NPCTextTimer
@@ -4278,6 +4289,8 @@ _npc_line_brk:
 	DEX
 	LDA NPC_VL_ByLine,X
 	STA <Map_NPCMsg_VL
+	LDA NPC_VH_ByLine,X
+	STA <Map_NPCMsg_VH
 
 _npc_do_character:
 	; Map_NPCTextTimer = 1
