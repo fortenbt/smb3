663e5c1bf7594b7415ebca34992672d7e9612595
fortenbt <1106428+fortenbt@users.noreply.github.com>
2020-08-05 20:11:46 -0400

    Fix fireball hitting bowser updated hitbox

--- PRG/prg007.asm
+++ PRG/prg007.asm
@@ -1345,7 +1345,7 @@ PRG007_A66C:
 Projectile_BBoxY:	.byte $18, $18, $28, $18, $18, $2c, $18, $18, $18, $18, $20, $18, $18, $28, $18, $18
 
 	; An X range per bounding box index
-Projectile_BBoxX:	.byte $10, $10, $10, $10, $10, $10, $10, $10, $10, $10, $10, $10, $10, $18, $10, $10
+Projectile_BBoxX:	.byte $10, $10, $10, $10, $10, $18, $10, $10, $10, $10, $10, $10, $10, $18, $10, $10
 
 
 PlayerProj_HitObject:
