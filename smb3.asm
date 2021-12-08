18e819f1b1531a106a54903eb874160ddcf4e868
fortenbt <1106428+fortenbt@users.noreply.github.com>
2021-12-08 12:06:51 -0500

    Fix ROM size based on header

--- smb3.asm
+++ smb3.asm
@@ -1,5 +1,5 @@
-	.inesprg 32  ; 16x 16KB PRG code (32 banks of 8KB)
-	.ineschr 32  ; 16x  8KB CHR data (128 banks of 1KB)
+	.inesprg 32  ; 32x 16KB PRG code (64 banks of 8KB)
+	.ineschr 32  ; 32x  8KB CHR data (256 banks of 1KB)
 	.inesmap 4   ; mapper 4 = MMC3, 8KB PRG, 1/2KB CHR bank swapping
 	.inesmir 0   ; background mirroring
 
@@ -5076,3 +5076,4 @@ TILE18_BOUNCEDBLOCK	= $C2	; Temporary tile for when block has been bounced
 	.incchr "CHR/chr138.pcx"
 	.incchr "CHR/chr139.pcx"
 
+	.bank 95	; fill out the ROM
